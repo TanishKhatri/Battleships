5e554becfc0b5457582ef4a8431f0b7001af878e
Tanish Khatri <tanishkhatriofficial@gmail.com>
2026-01-24 20:13:47 +0000

    feat<Player>: Player class added along with tests

---
+++ src/player.js
@@ -0,0 +1,13 @@
+import Gameboard from './gameboard.js';
+
+class Player {
+  id;
+  gameboard;
+
+  constructor() {
+    this.id = crypto.randomUUID();
+    this.gameboard = new Gameboard();
+  } 
+}
+
+export default Player;
