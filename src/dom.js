0f580260558667d9a6ea7634ff2006a35f155ebd
Tanish Khatri <tanishkhatriofficial@gmail.com>
2026-01-26 17:46:48 +0000

    Fix<titleScreen>: Arrow EL declarations
    Arrow Event Listener declarations are now global

--- src/dom.js
+++ src/dom.js
@@ -7,39 +7,40 @@ function renderTitleScreen() {
   body.appendChild(titleScreen);
 }
 
-function arrowKeysTitleScreenListeners() {
-  const upArrow = document.querySelector(".upArrow");
-  const leftArrow = document.querySelector(".leftArrow");
-  const downArrow = document.querySelector(".downArrow");
-  const rightArrow = document.querySelector(".rightArrow");
-  const enterKey = document.querySelector(".enterKey");
-  document.addEventListener("keydown", (e) => {
-    if (e.key === "ArrowUp") {
-      upArrow.classList.add("activated");
-    } else if (e.key === "ArrowLeft") {
-      leftArrow.classList.add("activated");
-    } else if (e.key === "ArrowDown") {
-      downArrow.classList.add("activated");
-    } else if (e.key === "ArrowRight") {
-      rightArrow.classList.add("activated");
-    } else if (e.key === "Enter") {
-      enterKey.classList.add("activated");
-    }
-  });
-
-  document.addEventListener("keyup", (e) => {
-    if (e.key === "ArrowUp") {
-      upArrow.classList.remove("activated");
-    } else if (e.key === "ArrowLeft") {
-      leftArrow.classList.remove("activated");
-    } else if (e.key === "ArrowDown") {
-      downArrow.classList.remove("activated");
-    } else if (e.key === "ArrowRight") {
-      rightArrow.classList.remove("activated");
-    } else if (e.key === "Enter") {
-      enterKey.classList.remove("activated");
-    }
-  });
-}
 renderTitleScreen();
-arrowKeysTitleScreenListeners();
+ 
+const upArrow = document.querySelector(".upArrow");
+const leftArrow = document.querySelector(".leftArrow");
+const downArrow = document.querySelector(".downArrow");
+const rightArrow = document.querySelector(".rightArrow");
+const enterKey = document.querySelector(".enterKey");
+
+function arrowEnterAddFunction(e) {
+if (e.key === "ArrowUp") {
+  upArrow.classList.add("activated");
+} else if (e.key === "ArrowLeft") {
+  leftArrow.classList.add("activated");
+} else if (e.key === "ArrowDown") {
+  downArrow.classList.add("activated");
+} else if (e.key === "ArrowRight") {
+  rightArrow.classList.add("activated");
+} else if (e.key === "Enter") {
+  enterKey.classList.add("activated");
+}
+}
+
+function arrrowEnterRemoveFunction(e) {
+  if (e.key === "ArrowUp") {
+    upArrow.classList.remove("activated");
+  } else if (e.key === "ArrowLeft") {
+    leftArrow.classList.remove("activated");
+  } else if (e.key === "ArrowDown") {
+    downArrow.classList.remove("activated");
+  } else if (e.key === "ArrowRight") {
+    rightArrow.classList.remove("activated");
+  } else if (e.key === "Enter") {
+    enterKey.classList.remove("activated");
+  }
+}
+document.addEventListener("keydown", arrowEnterAddFunction);
+document.addEventListener("keyup", arrrowEnterRemoveFunction);
